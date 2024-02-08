859756eea34c71ec19ea931916600055e340d8aa
Conspiciens <fernandezjohann08@gmail.com>
2024-02-08 13:13:24 -0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,20 +7,27 @@ import { Card, CardMedia, Typography } from "@mui/material";
 
 export default function Home() {
   return (
+  <div style={{ position: 'relative', height: '100vh' }}> 
     <Card
       sx={{
         width: "100%", 
         height: "100%", 
-        position: "relative",
+        position: "absolute",
       }}
       >
       <CardMedia 
         component="img"
-        image="/sunset_starship.jpg"/>
+        image="/sunset_starship.jpg"
+        sx = {{height: "100%", backgroundSize: 'cover'}}/>
       <div style={{position: "absolute", color: "white", top: 10, left: "50%",
-      transform: "translateX(-50%)"}}> 
+      transform: "translateX(-50%)", fontFamily: "monospace", fontSize: "15px"}}> 
           Aspiring Software/Machine Learning/Cybersecurity Engineer
       </div>
+      <div style={{position: "absolute", color: "#F2F0DF", top: 75, left: "50%",
+      transform: "translateX(-50%)", fontFamily: "monospace", fontSize: "80px", fontWeight: "bold"}}>
+        Johann Fernandez
+      </div>
     </Card>
+  </div>
   )
 }
