a3cc0a55bbc3d83dbc1a5bfc5e6290fb86f2f865
Conspiciens <fernandezjohann08@gmail.com>
2024-02-07 18:23:59 -0800

    Added Starship image to front page with text overlay

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
 import Image from "next/image";
 import Navbar from "./navbar";
 import about from "./about/page";
+import Box from '@mui/material/Box'; 
+import Grid from '@mui/material/Grid'; 
+import { Card, CardMedia, Typography } from "@mui/material";
 
 export default function Home() {
   return (
-
-    <p>Home Page</p>
+    <Card
+      sx={{
+        width: "100%", 
+        height: "100%", 
+        position: "relative",
+      }}
+      >
+      <CardMedia 
+        component="img"
+        image="/sunset_starship.jpg"/>
+      <div style={{position: "absolute", color: "white", top: 10, left: "50%",
+      transform: "translateX(-50%)"}}> 
+          Aspiring Software/Machine Learning/Cybersecurity Engineer
+      </div>
+    </Card>
   )
 }
