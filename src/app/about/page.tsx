899061a1121b65a1903af972633ff040b067b5bb
Conspiciens <fernandezjohann08@gmail.com>
2025-05-19 13:53:09 -0700

    Reverted changes due to issues with overlay text for other pages

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,7 +4,28 @@ const About = () => {
 
     return (
       <>
-        <p> About page </p>
+      <div style={{ 
+        position: 'relative', 
+        height: '100vh', 
+        }}> 
+        <Card sx={{
+          width: "100%", 
+          height: "100%", 
+          position: "absolute",
+          background: 'rgb(0, 0, 0, 0.6)', 
+        }}>
+          <CardMedia
+            component="img"
+            image="/sunset_starship.jpg"
+            sx = {{
+              height: "100%", 
+              backgroundSize: 'cover', 
+              backgroundRepeat: 'no-repeat',
+              filter: 'blur(3px)'
+            }}
+          />
+        </Card>
+      </div> 
       </>
     ); 
 }
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,7 +3,28 @@ import {Card, CardMedia} from '@mui/material';
 export default function Projects() {
     return (
         <> 
-            <p> Projects </p>
+            <div style={{ 
+              position: 'relative', 
+              height: '100vh', 
+              }}> 
+              <Card sx={{
+                width: "100%", 
+                height: "100%", 
+                position: "absolute",
+                background: 'rgb(0, 0, 0, 0.6)', 
+              }}>
+                <CardMedia
+                  component="img"
+                  image="/sunset_starship.jpg"
+                  sx = {{
+                    height: "100%", 
+                    backgroundSize: 'cover', 
+                    backgroundRepeat: 'no-repeat',
+                    filter: 'blur(3px)'
+                  }}
+                />
+              </Card>
+            </div> 
         </>
     )
 }
