7c7bd019e417a8a6789eec485d79f6368b573a3a
Conspiciens <fernandezjohann08@gmail.com>
2025-05-19 13:31:48 -0700

    Change background to blurred starship

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,40 +1,32 @@
-import { Box, Grid } from '@mui/material';
+import { Box, Card, CardMedia, Grid } from '@mui/material';
 
 const About = () => {
-    return(
-    <>
-      <div style={{ position: 'relative', marginTop: '50px'}}> 
-        <Grid 
-          container direction="row" 
-          justifyContent="flex-end" 
-          alignItems="center"> 
-          <Box component="span" sx={{
-            width: '40%',
-            display: 'block', 
-            p: 1,  
-            m: 1, 
-            border: '1px solid', 
-            borderRadius: 2,
-            fontSize: '0.875rem',
-            fontWeight: '700',
+
+    return (
+      <>
+        <div style={{ 
+          position: 'relative', 
+          height: '100vh', 
           }}> 
-              Johann Fernandez is an aspiring Software/Machine Learning/Cybersecurity engineer. When given his first 
-              Python book he learn to code in his spare time. From there he tinkered with various projects and even 
-              took time to learn about Cybersecurity. From here he expanded his knowledge fields to other domains in programming 
-              such as MySQL, Flask, C++, AWS, C, Embedded Systems, Linux and Machine Learning. Johann is currently working on numerous projects 
-              that can be found on this website. Such as an Flutter Application that uses Google Maps and Firebase for his backend. 
-              Johann at his Internship at Endera Motors worked on serveral applications that required usiing S3 and Lambda from AWS. 
-              Furthermore, he used Qt to ship a customized touch screen application for the Vehicle with Open Street Maps. Hes also 
-              experienced with Angular and TypeScript to write websites for engineers. Johann aspires to beocme a experience Software Engineer 
-              or Machine Learning Engineer, whatever it takes. Aspiring to work for revolutionary projects that push humanity foward to 
-              the stars.
-
-
-          </Box>
-          <img src="linkedin-3-64.png" style = {{ position: "relative"}} />
-        </Grid>
-      </div> 
-    </>
+          <Card sx={{
+            width: "100%", 
+            height: "100%", 
+            position: "absolute",
+            background: 'rgb(0, 0, 0, 0.6)', 
+          }}>
+            <CardMedia
+              component="img"
+              image="/sunset_starship.jpg"
+              sx = {{
+                height: "100%", 
+                backgroundSize: 'cover', 
+                backgroundRepeat: 'no-repeat',
+                filter: 'blur(3px)'
+              }}
+            />
+          </Card>
+        </div> 
+      </>
     ); 
 }
 
